5a585904ece8aa0ece2f3bfe94cec36bb4203f55
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2020-12-10 21:15:08 -0300

    add ckeditor text field

---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fa6374fc-74d2-45dd-9714-9e8a62d1dafd'
+};
---
+++ plugins/wysiwyg/admin/src/index.js
@@ -0,0 +1,52 @@
+import pluginPkg from '../../package.json';
+import pluginId from './pluginId';
+import App from './containers/App';
+import Initializer from './containers/Initializer';
+import lifecycles from './lifecycles';
+import trads from './translations';
+import Wysiwyg from './components/Wysiwyg';
+
+export default strapi => {
+  const pluginDescription = pluginPkg.strapi.description || pluginPkg.description;
+  const icon = pluginPkg.strapi.icon;
+  const name = pluginPkg.strapi.name;
+
+  const plugin = {
+    blockerComponent: null,
+    blockerComponentProps: {},
+    description: pluginDescription,
+    icon: pluginPkg.strapi.icon,
+    id: pluginId,
+    initializer: () => null,
+    injectedComponents: [],
+    isReady: true,
+    isRequired: pluginPkg.strapi.required || false,
+    mainComponent: null,
+    name: pluginPkg.strapi.name,
+    preventComponentRendering: false,
+    settings: null,
+    trads: {},
+    menu: {
+      pluginsSectionLinks: [
+        {
+          destination: `/plugins/${pluginId}`,
+          icon,
+          label: {
+            id: `${pluginId}.plugin.name`,
+            defaultMessage: name,
+          },
+          name,
+          permissions: [
+            // Uncomment to set the permissions of the plugin here
+            // {
+            //   action: '', // the action name should be plugins::plugin-name.actionType
+            //   subject: null,
+            // },
+          ],
+        },
+      ],
+    },
+  };
+  strapi.registerField({ type: 'wysiwyg', Component: Wysiwyg });
+  return strapi.registerPlugin(plugin);
+};
